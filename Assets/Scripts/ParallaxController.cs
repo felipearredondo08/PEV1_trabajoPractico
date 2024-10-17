8c2b8493f2ab265b629dabc376c9b49165b82c41
felipe_facultad <hellvilleskateshop@gmail.com>
2024-10-17 20:01:11 -0300

    Fondo Continuo en 3d mesh

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    public Rigidbody2D playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
