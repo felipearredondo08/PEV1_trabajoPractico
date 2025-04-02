7c010ef14af6affcf05f2b0e0a43e340ba259352
felipe <hellvilleskateshop@gmail.com>
2025-04-02 19:53:08 -0300

    Escena Cinematica casi terminada

--- Assets/Scripts/DialogoDeA3.cs
+++ Assets/Scripts/DialogoDeA3.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System.Collections;
@@ -143,4 +143,336 @@ public class DialogoNuevo : MonoBehaviour
         dialogueText.gameObject.SetActive(false);
         dialogueBackground.gameObject.SetActive(false);
     }
+}*/
+
+
+
+
+
+/*using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogoNuevo : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private Image[] characterImages;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip typingSound;
+    [SerializeField] private GameObject areaDialogoInicialCamara;
+    
+    [SerializeField] private GameObject dialogueBackgroundFirst;
+    [SerializeField] private GameObject dialogueBackgroundSecond;
+    
+    [TextArea(3, 5)]
+    [SerializeField] private string[] dialogueLinesFirst = {
+        "Bienvenido al primer nivel.",
+        "Debes recoger todas las monedas \npara completar el desafío.",
+        "¡Buena suerte, aventurero!"
+    };
+
+    [TextArea(3, 5)]
+    [SerializeField] private string[] dialogueLinesSecond = {
+        "¡Buen trabajo!",
+        "Ahora prepárate para el siguiente desafío.",
+        "Recuerda, la práctica hace al maestro."
+    };
+
+    [SerializeField] private float textSpeed = 0.05f;
+    [SerializeField] private float delayBetweenLines = 2f;
+    [SerializeField] private float fadeDuration = 1f;
+    [SerializeField] private float waitBeforeSecondDialogue = 3f;
+
+    private int currentCharacterIndex = 0;
+
+    private void Start()
+    {
+        StartCoroutine(HandleDialogues());
+    }
+
+    private IEnumerator HandleDialogues()
+    {
+        yield return StartCoroutine(DisplayDialogue(dialogueLinesFirst, dialogueBackgroundFirst));
+
+        if (dialogueBackgroundFirst != null)
+        {
+            Destroy(dialogueBackgroundFirst);
+        }
+
+        yield return new WaitForSeconds(waitBeforeSecondDialogue);
+        
+        if (dialogueBackgroundSecond != null)
+        {
+            dialogueBackgroundSecond.SetActive(true);
+        }
+
+        yield return StartCoroutine(DisplayDialogue(dialogueLinesSecond, dialogueBackgroundSecond));
+
+        if (areaDialogoInicialCamara != null)
+        {
+            Destroy(areaDialogoInicialCamara);
+        }
+    }
+
+    private IEnumerator DisplayDialogue(string[] dialogueLines, GameObject dialogueBackground)
+    {
+        if (dialogueBackground != null)
+        {
+            dialogueBackground.SetActive(true);
+        }
+        
+        dialogueText.gameObject.SetActive(true);
+        
+        foreach (var image in characterImages)
+        {
+            image.gameObject.SetActive(false);
+        }
+
+        characterImages[currentCharacterIndex].gameObject.SetActive(true);
+
+        for (int i = 0; i < dialogueLines.Length; i++)
+        {
+            characterImages[currentCharacterIndex].gameObject.SetActive(true);
+            yield return StartCoroutine(FadeInOutAnimation(true));
+            yield return StartCoroutine(TypeLine(dialogueLines[i]));
+            yield return new WaitForSeconds(delayBetweenLines);
+            yield return StartCoroutine(FadeInOutAnimation(false));
+
+            currentCharacterIndex = (currentCharacterIndex + 1) % characterImages.Length;
+
+            foreach (var image in characterImages)
+            {
+                image.gameObject.SetActive(false);
+            }
+            characterImages[currentCharacterIndex].gameObject.SetActive(true);
+        }
+
+        yield return StartCoroutine(FadeOutTextAndBackground(dialogueBackground));
+    }
+
+    private IEnumerator TypeLine(string line)
+    {
+        dialogueText.text = "";
+        dialogueText.color = new Color(dialogueText.color.r, dialogueText.color.g, dialogueText.color.b, 1);
+
+        foreach (char letter in line.ToCharArray())
+        {
+            dialogueText.text += letter;
+
+            if (audioSource != null && typingSound != null)
+            {
+                audioSource.PlayOneShot(typingSound);
+            }
+
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    private IEnumerator FadeInOutAnimation(bool fadeIn)
+    {
+        float elapsedTime = 0f;
+        Color startColor = characterImages[currentCharacterIndex].color;
+        float startAlpha = fadeIn ? 0f : startColor.a;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, fadeIn ? 1f : 0f, elapsedTime / fadeDuration);
+            characterImages[currentCharacterIndex].color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            yield return null;
+        }
+
+        Color finalColor = characterImages[currentCharacterIndex].color;
+        characterImages[currentCharacterIndex].color = new Color(finalColor.r, finalColor.g, finalColor.b, fadeIn ? 1f : 0f);
+    }
+
+    private IEnumerator FadeOutTextAndBackground(GameObject dialogueBackground)
+    {
+        float elapsedTime = 0f;
+        Color originalTextColor = dialogueText.color;
+        
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            dialogueText.color = new Color(originalTextColor.r, originalTextColor.g, originalTextColor.b, alpha);
+            yield return null;
+        }
+
+        dialogueText.gameObject.SetActive(false);
+        
+        if (dialogueBackground != null)
+        {
+            dialogueBackground.SetActive(false);
+        }
+    }
+}*/
+
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogoNuevo : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    
+    // Imágenes de los personajes para el primer diálogo
+    [SerializeField] private Image[] characterImagesFirst;
+    
+    // Imágenes de los personajes para el segundo diálogo
+    [SerializeField] private Image[] characterImagesSecond;
+    
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip typingSound;
+    [SerializeField] private GameObject areaDialogoInicialCamara;
+    
+    [SerializeField] private GameObject dialogueBackgroundFirst;
+    [SerializeField] private GameObject dialogueBackgroundSecond;
+    
+    [TextArea(3, 5)]
+    [SerializeField] private string[] dialogueLinesFirst = {
+        "Bienvenido al primer nivel.",
+        "Debes recoger todas las monedas \npara completar el desafío.",
+        "¡Buena suerte, aventurero!"
+    };
+
+    [TextArea(3, 5)]
+    [SerializeField] private string[] dialogueLinesSecond = {
+        "¡Buen trabajo!",
+        "Ahora prepárate para el siguiente desafío.",
+        "Recuerda, la práctica hace al maestro."
+    };
+
+    [SerializeField] private float textSpeed = 0.05f;
+    [SerializeField] private float delayBetweenLines = 2f;
+    [SerializeField] private float fadeDuration = 1f;
+    [SerializeField] private float waitBeforeSecondDialogue = 3f;
+
+    private void Start()
+    {
+        StartCoroutine(HandleDialogues());
+    }
+
+    private IEnumerator HandleDialogues()
+    {
+        yield return StartCoroutine(DisplayDialogue(dialogueLinesFirst, dialogueBackgroundFirst, characterImagesFirst));
+
+        if (dialogueBackgroundFirst != null)
+        {
+            Destroy(dialogueBackgroundFirst);
+        }
+
+        yield return new WaitForSeconds(waitBeforeSecondDialogue);
+        
+        if (dialogueBackgroundSecond != null)
+        {
+            dialogueBackgroundSecond.SetActive(true);
+        }
+
+        yield return StartCoroutine(DisplayDialogue(dialogueLinesSecond, dialogueBackgroundSecond, characterImagesSecond));
+
+        if (areaDialogoInicialCamara != null)
+        {
+            Destroy(areaDialogoInicialCamara);
+        }
+    }
+
+    private IEnumerator DisplayDialogue(string[] dialogueLines, GameObject dialogueBackground, Image[] characterImages)
+    {
+        if (dialogueBackground != null)
+        {
+            dialogueBackground.SetActive(true);
+        }
+        
+        dialogueText.gameObject.SetActive(true);
+
+        // Desactivar todas las imágenes de personajes antes de empezar
+        foreach (var image in characterImages)
+        {
+            image.gameObject.SetActive(false);
+        }
+
+        int currentCharacterIndex = 0;
+        characterImages[currentCharacterIndex].gameObject.SetActive(true);
+
+        for (int i = 0; i < dialogueLines.Length; i++)
+        {
+            characterImages[currentCharacterIndex].gameObject.SetActive(true);
+            yield return StartCoroutine(FadeInOutAnimation(characterImages[currentCharacterIndex], true));
+            yield return StartCoroutine(TypeLine(dialogueLines[i]));
+            yield return new WaitForSeconds(delayBetweenLines);
+            yield return StartCoroutine(FadeInOutAnimation(characterImages[currentCharacterIndex], false));
+
+            // Cambia de personaje para la siguiente línea
+            currentCharacterIndex = (currentCharacterIndex + 1) % characterImages.Length;
+
+            foreach (var image in characterImages)
+            {
+                image.gameObject.SetActive(false);
+            }
+            characterImages[currentCharacterIndex].gameObject.SetActive(true);
+        }
+
+        yield return StartCoroutine(FadeOutTextAndBackground(dialogueBackground));
+    }
+
+    private IEnumerator TypeLine(string line)
+    {
+        dialogueText.text = "";
+        dialogueText.color = new Color(dialogueText.color.r, dialogueText.color.g, dialogueText.color.b, 1);
+
+        foreach (char letter in line.ToCharArray())
+        {
+            dialogueText.text += letter;
+
+            if (audioSource != null && typingSound != null)
+            {
+                audioSource.PlayOneShot(typingSound);
+            }
+
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    private IEnumerator FadeInOutAnimation(Image characterImage, bool fadeIn)
+    {
+        float elapsedTime = 0f;
+        Color startColor = characterImage.color;
+        float startAlpha = fadeIn ? 0f : startColor.a;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, fadeIn ? 1f : 0f, elapsedTime / fadeDuration);
+            characterImage.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            yield return null;
+        }
+
+        Color finalColor = characterImage.color;
+        characterImage.color = new Color(finalColor.r, finalColor.g, finalColor.b, fadeIn ? 1f : 0f);
+    }
+
+    private IEnumerator FadeOutTextAndBackground(GameObject dialogueBackground)
+    {
+        float elapsedTime = 0f;
+        Color originalTextColor = dialogueText.color;
+        
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            dialogueText.color = new Color(originalTextColor.r, originalTextColor.g, originalTextColor.b, alpha);
+            yield return null;
+        }
+
+        dialogueText.gameObject.SetActive(false);
+        
+        if (dialogueBackground != null)
+        {
+            dialogueBackground.SetActive(false);
+        }
+    }
 }
+
--- Assets/Scripts/ZoomComienzoAnimacionNivel4.cs
+++ Assets/Scripts/ZoomComienzoAnimacionNivel4.cs
@@ -1,17 +1,196 @@
-using UnityEngine;
+/*using UnityEngine;
 using Cinemachine;
+using System.Collections;
 
-public class CameraZoomTrigger : MonoBehaviour
+public class ZoomComienzoAnimacionNivel4 : MonoBehaviour
 {
     public CinemachineVirtualCamera virtualCamera;
-    public float zoomAmount = 3f; // Más bajo = más zoom
+    public Transform player;
 
-    private void OnTriggerEnter(Collider other)
+    [Header("Screen X Settings")]
+    public float targetScreenX = 0.22f;
+    public float resetScreenX = 0.5f;
+    public float finalScreenX = 0.4f; // Posición final después de 35s
+
+    [Header("Height Adjustment (Y)")]
+    public float targetOffsetY = 2f;
+    public float resetOffsetY = 0f;
+    public float finalOffsetY = 1f; // Posición final en Y después de 35s
+
+    [Header("Zoom Settings")]
+    public float zoomOutAmount = 0.2f;
+    private float defaultZoom;
+
+    [Header("Speed Settings")]
+    public float detectionThreshold = 0.1f;
+    public float transitionSpeed = 2f;
+
+    private CinemachineFramingTransposer framingTransposer;
+    private bool isAdjusted = false;
+
+    private void Start()
     {
-        if (other.CompareTag("Player"))
+        framingTransposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        defaultZoom = virtualCamera.m_Lens.OrthographicSize;
+
+        // Aseguramos que la cámara comience en la posición inicial deseada
+        framingTransposer.m_ScreenX = resetScreenX;
+        framingTransposer.m_TrackedObjectOffset.y = resetOffsetY;
+
+        StartCoroutine(AdjustCameraAfterTime());
+    }
+
+    private void Update()
+    {
+        float velocityX = player.GetComponent<Rigidbody2D>().velocity.x;
+
+        if (!isAdjusted && Mathf.Abs(velocityX) < detectionThreshold)
         {
-            Debug.Log("✅ Activando zoom...");
-            virtualCamera.m_Lens.OrthographicSize = zoomAmount;
+            isAdjusted = true;
+            StopAllCoroutines();
+            StartCoroutine(AdjustCamera(targetScreenX, targetOffsetY));
+        }
+        else if (isAdjusted && Mathf.Abs(velocityX) > detectionThreshold)
+        {
+            isAdjusted = false;
+            StopAllCoroutines();
+            StartCoroutine(AdjustCamera(resetScreenX, resetOffsetY));
         }
     }
+
+    private IEnumerator AdjustCamera(float newScreenX, float newOffsetY)
+    {
+        while (Mathf.Abs(framingTransposer.m_ScreenX - newScreenX) > 0.01f ||
+               Mathf.Abs(framingTransposer.m_TrackedObjectOffset.y - newOffsetY) > 0.01f)
+        {
+            framingTransposer.m_ScreenX = Mathf.Lerp(framingTransposer.m_ScreenX, newScreenX, Time.deltaTime * transitionSpeed);
+            framingTransposer.m_TrackedObjectOffset.y = Mathf.Lerp(framingTransposer.m_TrackedObjectOffset.y, newOffsetY, Time.deltaTime * transitionSpeed);
+            yield return null;
+        }
+
+        framingTransposer.m_ScreenX = newScreenX;
+        framingTransposer.m_TrackedObjectOffset.y = newOffsetY;
+    }
+
+    private IEnumerator AdjustCameraAfterTime()
+    {
+        yield return new WaitForSeconds(35f);
+        Debug.Log("🔹 Pasaron 35s: Ajustando cámara final...");
+
+        StartCoroutine(SmoothAdjustScreenX(finalScreenX));
+        StartCoroutine(SmoothAdjustOffsetY(finalOffsetY));
+        StartCoroutine(SmoothAdjustZoom(defaultZoom + zoomOutAmount));
+    }
+
+    private IEnumerator SmoothAdjustScreenX(float newScreenX)
+    {
+        while (Mathf.Abs(framingTransposer.m_ScreenX - newScreenX) > 0.01f)
+        {
+            framingTransposer.m_ScreenX = Mathf.Lerp(framingTransposer.m_ScreenX, newScreenX, Time.deltaTime * transitionSpeed);
+            yield return null;
+        }
+        framingTransposer.m_ScreenX = newScreenX;
+    }
+
+    private IEnumerator SmoothAdjustOffsetY(float newOffsetY)
+    {
+        while (Mathf.Abs(framingTransposer.m_TrackedObjectOffset.y - newOffsetY) > 0.01f)
+        {
+            framingTransposer.m_TrackedObjectOffset.y = Mathf.Lerp(framingTransposer.m_TrackedObjectOffset.y, newOffsetY, Time.deltaTime * transitionSpeed);
+            yield return null;
+        }
+        framingTransposer.m_TrackedObjectOffset.y = newOffsetY;
+    }
+
+    private IEnumerator SmoothAdjustZoom(float targetZoom)
+    {
+        while (Mathf.Abs(virtualCamera.m_Lens.OrthographicSize - targetZoom) > 0.01f)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, targetZoom, Time.deltaTime * transitionSpeed);
+            yield return null;
+        }
+        virtualCamera.m_Lens.OrthographicSize = targetZoom;
+    }
+}*/
+
+using UnityEngine;
+using Cinemachine;
+using System.Collections;
+
+public class ZoomComienzoAnimacionNivel4 : MonoBehaviour
+{
+    public CinemachineVirtualCamera virtualCamera;
+    public Transform player;
+
+    [Header("Screen X Settings")]
+    public float initialScreenX = 0.22f;  // Donde inicia (esquina)
+    public float finalScreenX = 0.5f;     // Donde debe quedar después de 35s (centrado)
+
+    [Header("Height Adjustment (Y)")]
+    public float initialOffsetY = 2f;  // Altura inicial
+    public float finalOffsetY = 0f;    // Altura después de 35s
+
+    [Header("Zoom Settings")]
+    public float initialZoom = 5f;  // Zoom inicial
+    public float finalZoom = 5.2f;  // Zoom después de 35s (más alejado)
+
+    [Header("Speed Settings")]
+    public float transitionSpeed = 2f;
+
+    private CinemachineFramingTransposer framingTransposer;
+
+    private void Start()
+    {
+        framingTransposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+
+        // Configurar la cámara en la posición inicial correcta
+        framingTransposer.m_ScreenX = initialScreenX;
+        framingTransposer.m_TrackedObjectOffset.y = initialOffsetY;
+        virtualCamera.m_Lens.OrthographicSize = initialZoom;
+
+        // Iniciar la transición después de 35 segundos
+        StartCoroutine(AdjustCameraAfterTime());
+    }
+
+    private IEnumerator AdjustCameraAfterTime()
+    {
+        yield return new WaitForSeconds(35f);
+        Debug.Log("🔹 Pasaron 35s: Ajustando cámara a posición final...");
+
+        // Iniciar la transición a la nueva posición
+        StartCoroutine(SmoothAdjustScreenX(finalScreenX));
+        StartCoroutine(SmoothAdjustOffsetY(finalOffsetY));
+        StartCoroutine(SmoothAdjustZoom(finalZoom));
+    }
+
+    private IEnumerator SmoothAdjustScreenX(float newScreenX)
+    {
+        while (Mathf.Abs(framingTransposer.m_ScreenX - newScreenX) > 0.01f)
+        {
+            framingTransposer.m_ScreenX = Mathf.Lerp(framingTransposer.m_ScreenX, newScreenX, Time.deltaTime * transitionSpeed);
+            yield return null;
+        }
+        framingTransposer.m_ScreenX = newScreenX;
+    }
+
+    private IEnumerator SmoothAdjustOffsetY(float newOffsetY)
+    {
+        while (Mathf.Abs(framingTransposer.m_TrackedObjectOffset.y - newOffsetY) > 0.01f)
+        {
+            framingTransposer.m_TrackedObjectOffset.y = Mathf.Lerp(framingTransposer.m_TrackedObjectOffset.y, newOffsetY, Time.deltaTime * transitionSpeed);
+            yield return null;
+        }
+        framingTransposer.m_TrackedObjectOffset.y = newOffsetY;
+    }
+
+    private IEnumerator SmoothAdjustZoom(float targetZoom)
+    {
+        while (Mathf.Abs(virtualCamera.m_Lens.OrthographicSize - targetZoom) > 0.01f)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(virtualCamera.m_Lens.OrthographicSize, targetZoom, Time.deltaTime * transitionSpeed);
+            yield return null;
+        }
+        virtualCamera.m_Lens.OrthographicSize = targetZoom;
+    }
 }
+
